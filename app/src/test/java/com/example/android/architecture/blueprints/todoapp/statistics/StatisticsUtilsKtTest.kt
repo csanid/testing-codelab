47c8dd658705b84630feb673279b83361d8c6373
csanid <cecisi89@gmail.com>
2023-12-26 12:13:40 -0300

    Add Hamcrest to stats test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
@@ -1,6 +1,8 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -11,7 +13,7 @@ class StatisticsUtilsKtTest {
             Task("title", "description", isCompleted = false)
         )
         val result = getActiveAndCompletedStats(tasks)
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
     }
 }
