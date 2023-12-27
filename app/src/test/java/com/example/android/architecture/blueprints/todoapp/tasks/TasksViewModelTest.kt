359d1534b8538efe61032bba5affbdbc7531ce96
csanid <cecisi89@gmail.com>
2023-12-27 11:23:39 -0300

    Add extension function for setting observer for LiveData tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -10,6 +12,8 @@ import org.robolectric.annotation.Config
 @Config(sdk = [30])
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
     @Test
     fun addNewTask_setsNewTaskEvent() {
         // Given a fresh TasksViewModel
